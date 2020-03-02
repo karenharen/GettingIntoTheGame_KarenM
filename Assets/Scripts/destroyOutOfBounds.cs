80bdae172048c70545d83fb68f7e6c2af1ae976e
Karen <karenharenschool@gmail.com>
2020-03-02 11:00:13 -0800

    Class 3-2
    added movement and destroy game objects

---
+++ Assets/Scripts/destroyOutOfBounds.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroyOutOfBounds : MonoBehaviour
+{ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int upperOutOfBounds = 30;
+        int lowerOutOfBounds = -6;
+
+        if (transform.position.z >= upperOutOfBounds || transform.position.z <= lowerOutOfBounds)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    public GameObject projectile;
+
+    const float horizontalBoundry = 10.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x > horizontalBoundry)
+        {
+            transform.position = new Vector3(horizontalBoundry, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < -horizontalBoundry)
+        {
+            transform.position = new Vector3(-horizontalBoundry, transform.position.y, transform.position.z);
+        }
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectile, transform.position, projectile.transform.rotation);
+        }
+
+    }
+}
