a5fba6292215e3aa03120830374eb6dd8b6cdda4
CBTE Account <cbte@ac.sdmesa.net>
2020-03-04 11:29:14 -0800

    commit for 3-4 class

---
+++ Assets/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemyAnimals;
+
+    const float xRange = 10;
+    const float spawnPointZ = 25;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            int randomAnimal = Random.Range(0, enemyAnimals.Length);            
+            Instantiate(enemyAnimals[randomAnimal], new Vector3(Random.Range(-xRange, xRange), 0, spawnPointZ), enemyAnimals[randomAnimal].transform.rotation);
+        }
+        
+    }
+}
